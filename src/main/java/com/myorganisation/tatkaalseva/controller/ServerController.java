f95947c16fd8b272c46782b575c23d4a08447c94
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-04-03 17:52:59 +0530

    Initial commit

---
+++ src/main/java/com/myorganisation/tatkaalseva/controller/ServerController.java
@@ -0,0 +1,18 @@
+package com.myorganisation.tatkaalseva.controller;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ServerController {
+
+    @GetMapping
+    public ResponseEntity<String> serverHealth() {
+        return new ResponseEntity<>("Server is live!", HttpStatusCode.valueOf(200));
+
+    }
+}
