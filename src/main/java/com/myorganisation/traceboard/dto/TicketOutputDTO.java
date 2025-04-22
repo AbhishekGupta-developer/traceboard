81f96cd8719fa5dffd8536861a93eaf27389326b
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-04-22 18:08:56 +0530

    Add API endpoint for Ticket creation

--- src/main/java/com/myorganisation/traceboard/dto/TicketOutputDTO.java
+++ src/main/java/com/myorganisation/traceboard/dto/TicketOutputDTO.java
@@ -3,9 +3,11 @@ package com.myorganisation.traceboard.dto;
 import com.myorganisation.traceboard.model.enums.TicketCategory;
 import com.myorganisation.traceboard.model.enums.TicketPriority;
 import com.myorganisation.traceboard.model.enums.TicketStatus;
+import lombok.Data;
 
 import java.util.Date;
 
+@Data
 public class TicketOutputDTO {
 
     private Long id;
--- src/main/java/com/myorganisation/traceboard/service/TicketServiceImpl.java
+++ src/main/java/com/myorganisation/traceboard/service/TicketServiceImpl.java
@@ -5,10 +5,12 @@ import com.myorganisation.traceboard.dto.TicketOutputDTO;
 import com.myorganisation.traceboard.model.Ticket;
 import com.myorganisation.traceboard.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class TicketServiceImpl implements TicketService {
 
     @Autowired
@@ -28,6 +30,20 @@ public class TicketServiceImpl implements TicketService {
         ticket.setPriority(ticketInputDTO.getPriority());
 
         ticket = ticketRepository.save(ticket);
+
+        TicketOutputDTO ticketOutputDTO = new TicketOutputDTO();
+
+        ticketOutputDTO.setId(ticket.getId());
+        ticketOutputDTO.setName(ticket.getName());
+        ticketOutputDTO.setCreatedBy(ticket.getCreatedBy());
+        ticketOutputDTO.setAssignedTo(ticket.getAssignedTo());
+        ticketOutputDTO.setDescription(ticket.getDescription());
+        ticketOutputDTO.setDateCreated(ticket.getDateCreated());
+        ticketOutputDTO.setStatus(ticket.getStatus());
+        ticketOutputDTO.setCategory(ticket.getCategory());
+        ticketOutputDTO.setPriority(ticket.getPriority());
+
+        return ticketOutputDTO;
     }
 
     @Override
