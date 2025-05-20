750fd8e1c66c4c3422cfa7978097803cfccedc31
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-05-20 19:18:18 +0530

    Partial code commit in UserService interface

--- src/main/java/com/myorganisation/traceboard/service/UserService.java
+++ src/main/java/com/myorganisation/traceboard/service/UserService.java
@@ -2,10 +2,17 @@ package com.myorganisation.traceboard.service;
 
 import com.myorganisation.traceboard.dto.UserRequestDTO;
 import com.myorganisation.traceboard.dto.UserResponseDTO;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface UserService {
     UserResponseDTO registerUser(UserRequestDTO userRequestDTO, MultipartFile photo);
     UserResponseDTO getUser(Long id);
+    List<UserResponseDTO> getAllUsers();
+    UserResponseDTO updateUser(Long id, UserRequestDTO userRequestDTO);
+    String removeUser(Long id);
+    Page<UserResponseDTO> getUserPage(Integer page, Integer size, String sortBy, String orderBy);
     String getUserPhoto(Long id);
 }
