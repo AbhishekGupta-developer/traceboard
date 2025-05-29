c6dace5870c74083682dd1f1d6b5bd3620b08aca
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-05-29 18:07:29 +0530

    Add new cron job and modified previous one of every five seconds

--- src/main/java/com/myorganisation/traceboard/cron/Task.java
+++ src/main/java/com/myorganisation/traceboard/cron/Task.java
@@ -8,6 +8,12 @@ public class Task {
 
     @Scheduled(cron = "*/5 * * * * *")
     public void cronJob() {
-        System.out.println("Executes every seconds...");
+        System.out.println("Executes every 5 seconds...");
     }
+
+    @Scheduled(cron = "${my.cron.expression}")
+    public void cronJob2() {
+        System.out.println("Executes every seconds");
+    }
+
 }
