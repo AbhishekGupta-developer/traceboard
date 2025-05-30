d885cde5675bb6b7f6cae903a20bb82899b07f17
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-05-30 18:04:02 +0530

    Add ErrorResponseDTO

---
+++ src/main/java/com/myorganisation/traceboard/dto/ErrorResponseDTO.java
@@ -0,0 +1,22 @@
+package com.myorganisation.traceboard.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ErrorResponseDTO {
+    private LocalDateTime localDateTime;
+    private String message;
+    private String details;
+
+    public ErrorResponseDTO(
+            LocalDateTime localDateTime,
+            String message,
+            String details
+    ) {
+        this.localDateTime = localDateTime;
+        this.message = message;
+        this.details = details;
+    }
+}
