16db2fddebcfb7e730be47d911479f04bc7224de
Shivani-Developer89 <your-email@example.com>
2025-06-03 01:52:13 +0530

    Save all local changes before switching branches

--- src/main/java/com/myorganisation/traceboard/crons/Task.java
+++ src/main/java/com/myorganisation/traceboard/crons/Task.java
@@ -1,4 +1,13 @@
 package com.myorganisation.traceboard.crons;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Task {
+//    @Scheduled(cron = "*/5 * * * * *")
+//    public void cronJob() {
+//        System.out.println("Executes every 5 seconds...");
+//    }
+
 }
