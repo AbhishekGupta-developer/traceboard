ea1d5ae69b4516a7904d86720a3c14ff66ee00a7
Shivani-Developer89 <your-email@example.com>
2025-06-04 13:19:57 +0530

    changes in serviceimpl file

--- src/main/java/com/myorganisation/traceboard/cron/Task.java
+++ src/main/java/com/myorganisation/traceboard/cron/Task.java
@@ -4,7 +4,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Task {
+public class
+Task {
 
     @Scheduled(cron = "*/5 * * * * *")
     public void cronJob() {
