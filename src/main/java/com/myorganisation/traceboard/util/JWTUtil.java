ce8639833372bab1c56511a570301e389465da89
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-09 20:41:12 +0530

    Implement JWTUtil to generate JWT token

---
+++ src/main/java/com/myorganisation/traceboard/util/JWTUtil.java
@@ -0,0 +1,24 @@
+package com.myorganisation.traceboard.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    private final String SECRET = "OHYcZrZ7cWJHBlwMOPolGQdyddkSRZyYY++Wff3z6g4=";
+    private final SecretKey KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private final long EXPIRATION_TIME = 1000 * 60 * 5; //Valid for 5 mins
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
