0bcf48566153781e2bdbde60d8f14c2f6b9e9e8b
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-21 20:50:12 +0530

    Partial code commit of 'validateToken' method

--- src/main/java/com/myorganisation/traceboard/util/JWTUtil.java
+++ src/main/java/com/myorganisation/traceboard/util/JWTUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -32,5 +33,9 @@ public class JWTUtil {
                 .getBody();
 
         return body.getSubject();
+    }
+
+    public boolean validateToken(String username, UserDetails userDetails, String token) {
+        return false;
     }
 }
