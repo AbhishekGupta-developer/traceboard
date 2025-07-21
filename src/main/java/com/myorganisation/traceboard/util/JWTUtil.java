14ba8ecba17f2e67511ce81e5ad7a72fee9b5edf
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-21 20:33:43 +0530

    Add 'extractUsername' method

--- src/main/java/com/myorganisation/traceboard/util/JWTUtil.java
+++ src/main/java/com/myorganisation/traceboard/util/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.myorganisation.traceboard.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -13,6 +14,7 @@ public class JWTUtil {
     private final String SECRET = "OHYcZrZ7cWJHBlwMOPolGQdyddkSRZyYY++Wff3z6g4=";
     private final SecretKey KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
     private final long EXPIRATION_TIME = 1000 * 60 * 5; //Valid for 5 mins
+
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
@@ -20,5 +22,15 @@ public class JWTUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(KEY, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+        Claims body = Jwts.parser()
+                .setSigningKey(KEY)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+
+        return body.getSubject();
     }
 }
