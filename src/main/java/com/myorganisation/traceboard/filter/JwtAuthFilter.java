d193a42d8aeb529cec5f31951f19c31618d6e712
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-21 20:51:18 +0530

    Partial code commit of JwtAuthFilter

---
+++ src/main/java/com/myorganisation/traceboard/filter/JwtAuthFilter.java
@@ -0,0 +1,42 @@
+package com.myorganisation.traceboard.filter;
+
+import com.myorganisation.traceboard.service.UserServiceImpl;
+import com.myorganisation.traceboard.util.JWTUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        String token = null;
+        String username = null;
+
+        if(authHeader != null && authHeader.startsWith("Bearer")) {
+            token = authHeader.substring(7);
+            username = jwtUtil.extractUsername(token);
+        }
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userServiceImpl.loadUserByUsername(username);
+        }
+    }
+}
