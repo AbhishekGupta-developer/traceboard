e048defff646aaff42e5b3c36fa8b7569a765490
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-08-04 19:47:01 +0530

    Replace deprecated methods with new methods

--- src/main/java/com/myorganisation/traceboard/util/JWTUtil.java
+++ src/main/java/com/myorganisation/traceboard/util/JWTUtil.java
@@ -2,8 +2,9 @@ package com.myorganisation.traceboard.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +13,19 @@ import java.util.Date;
 
 @Component
 public class JWTUtil {
+
+    private final Logger logger = LoggerFactory.getLogger(JWTUtil.class);
+
     private final String SECRET = "OHYcZrZ7cWJHBlwMOPolGQdyddkSRZyYY++Wff3z6g4=";
     private final SecretKey KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
     private final long EXPIRATION_TIME = 1000 * 60 * 5; //Valid for 5 mins
 
     public String generateToken(String username) {
         return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(KEY, SignatureAlgorithm.HS256)
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(KEY, Jwts.SIG.HS256)
                 .compact();
     }
 
@@ -32,15 +36,15 @@ public class JWTUtil {
     }
 
     private Claims getClaims(String token) {
-        Claims body = Jwts.parser()
-                .setSigningKey(KEY)
+        return Jwts.parser()
+                .verifyWith(KEY)
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
-        return body;
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public boolean validateToken(String username, UserDetails userDetails, String token) {
+        logger.info("Inside JWTUtil -> validateToken | username: {} | UserDetails: {} | Token: {}", username, userDetails, token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
