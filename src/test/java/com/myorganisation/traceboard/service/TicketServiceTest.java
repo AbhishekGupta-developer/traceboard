44a15a90216f56cbd17a4adb7e660da5b21dddd0
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-08-13 20:57:55 +0530

    Add new test case

--- src/test/java/com/myorganisation/traceboard/service/TicketServiceTest.java
+++ src/test/java/com/myorganisation/traceboard/service/TicketServiceTest.java
@@ -8,8 +8,7 @@ import com.myorganisation.traceboard.model.enums.TicketCategory;
 import com.myorganisation.traceboard.model.enums.TicketPriority;
 import com.myorganisation.traceboard.model.enums.TicketStatus;
 import com.myorganisation.traceboard.repository.TicketRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -17,6 +16,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class TicketServiceTest {
@@ -27,8 +27,19 @@ public class TicketServiceTest {
     @InjectMocks
     TicketServiceImpl ticketService;
 
+    @BeforeAll
+    public static void methodToExecuteBeforeAllTest() {
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    public void methodToExecuteBeforeEachTest() {
+        System.out.println("Before Each");
+    }
+
     @Test
     public void createTicketShouldAddNewTicketInDB() {
+        System.out.println("Inside: createTicketShouldAddNewTicketInDB TEST");
         TicketRequestDTO ticketRequestDTO = new TicketRequestDTO();
         ticketRequestDTO.setName("JUnit Test Ticket");
         ticketRequestDTO.setCreatedBy(1L);
@@ -44,7 +55,8 @@ public class TicketServiceTest {
         ticket.setCreatedBy(1L);
         ticket.setAssignedTo(2L);
         ticket.setDescription("Its a test ticket to demonstrate JUnit and Mockito");
-        ticket.setDateCreated(new Date());
+        Date date = new Date();
+        ticket.setDateCreated(date);
         ticket.setStatus(TicketStatus.TO_DO);
         ticket.setCategory(TicketCategory.BUG);
         ticket.setPriority(TicketPriority.MAX);
@@ -57,7 +69,53 @@ public class TicketServiceTest {
 
         Assertions.assertNotNull(ticketResponseDTO);
         Assertions.assertEquals(ticket.getId(), ticketResponseDTO.getId());
+        Assertions.assertEquals("JUnit Test Ticket", ticketResponseDTO.getName());
+        Assertions.assertEquals(1L, ticketResponseDTO.getCreatedBy());
+        Assertions.assertEquals(2L, ticketResponseDTO.getAssignedTo());
+        Assertions.assertEquals("Its a test ticket to demonstrate JUnit and Mockito", ticketResponseDTO.getDescription());
+        Assertions.assertEquals(date, ticketResponseDTO.getDateCreated());
         Assertions.assertTrue(ticketResponseDTO.getStatus() == TicketStatus.TO_DO);
+        Assertions.assertEquals(TicketCategory.BUG, ticketResponseDTO.getCategory());
+        Assertions.assertEquals(TicketPriority.MAX, ticketResponseDTO.getPriority());
+        Assertions.assertNotNull(ticketResponseDTO.getInvoice());
+    }
+
+    @Test
+    public void getTicketShouldGiveTicket() {
+        System.out.println("Inside: getTicketShouldGiveTicket TEST");
+        Ticket ticket = new Ticket();
+        ticket.setId(1L);
+        ticket.setName("JUnit Test Ticket");
+        ticket.setCreatedBy(1L);
+        ticket.setAssignedTo(2L);
+        ticket.setDescription("Its a test ticket to demonstrate JUnit and Mockito");
+        ticket.setDateCreated(new Date());
+        ticket.setStatus(TicketStatus.TO_DO);
+        ticket.setCategory(TicketCategory.BUG);
+        ticket.setPriority(TicketPriority.MAX);
+        ticket.setInvoice(new Invoice());
+
+        Optional<Ticket> ticketOptional = Optional.of(ticket);
+
+        Mockito.when(ticketRepository.findById(Mockito.any(Long.class)))
+                .thenReturn(ticketOptional);
+
+        TicketResponseDTO ticketResponseDTO = ticketService.getTicket(1L);
+
+        Assertions.assertNotNull(ticketResponseDTO);
+        Assertions.assertEquals(1, ticketResponseDTO.getId());
+        Assertions.assertEquals(ticket.getId(), ticketResponseDTO.getId());
+        Assertions.assertTrue(ticketResponseDTO.getStatus() == TicketStatus.TO_DO);
+    }
+
+    @AfterEach
+    public void methodToExecuteAfterEachTest() {
+        System.out.println("After Each");
+    }
+
+    @AfterAll
+    public static void methodToExecuteAfterAllTest() {
+        System.out.println("After All");
     }
 
 }
